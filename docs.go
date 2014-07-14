9855b241d41a06be451393f2bd0b3ea7afb969d2
Richard Downer <richard.downer@xero.com>
2014-07-15 08:29:07 +1200

    some more on this

--- docs.go
+++ docs.go
@@ -6,26 +6,55 @@ import (
 	"os"
 )
 
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
 func main() {
 
-	appName := os.Args[1]
-	availableApps, _ := ioutil.ReadDir("./apps")
-	selectedApp := ""
+	const dir = "./apps"
+	currentDir := ""
 
+	appName := os.Args[1]
+	availableApps, _ := ioutil.ReadDir(dir)
+
+	// select an available app
 	for _, app := range availableApps {
 		if app.Name() == appName {
-			selectedApp = app.Name()
-			fmt.Println("Selected", selectedApp)
-		} else {
-			fmt.Println("No docs available for", app)
+			currentDir = dir + "/" + appName
+			// fmt.Println(currentDir)
+			break
 		}
 	}
 
-	var choice int
-	_, err := fmt.Scanf("%d", &choice)
-	if err != nil {
-		fmt.Println(err)
+	// app not found
+	if currentDir == "" {
+		fmt.Println(appName, "not found ...")
 		return
 	}
 
+	// load cmd choices
+	availableCmd, _ := ioutil.ReadDir(currentDir)
+	for _, cmd := range availableCmd {
+		fmt.Println(">", cmd.Name())
+	}
+
+	// get the users cmd choice
+	var choice string
+	_, err := fmt.Scanf("%s", &choice)
+	check(err)
+
+	// update current dir
+	currentDir = currentDir + "/" + choice
+
+	// display the chosen cmd doc
+	f, err := os.Open(currentDir)
+	check(err)
+
+	b1 := make([]byte, 1024)
+	n1, err := f.Read(b1)
+	check(err)
+	fmt.Printf("%d, %s", n1, string(b1))
 }
